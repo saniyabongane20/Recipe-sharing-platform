7ab0c9fec922a38cc239e9777036d50fcd65c4fe
saniyabongane20 <saniyabongane@gmail.com>
2025-02-23 12:33:12 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <img src="/3.jpg" class="img-fluid w-60 h-40 pt-1" alt="..."/ >
+     <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="/c5.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Pizza</h5>
+        <p class="card-text">"Good things come in circles… like this pizza! Enjoy!".</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/c2.jpeg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Donuts</h5>
+        <p class="card-text"> "Life is sweeter with desserts! Indulge in some delicious sweets!"</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/c3.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Biryani</h5>
+        <p class="card-text"> "Biryani is always the answer! Enjoy this delicious treat on me!".</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/c4.jpg" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Misal Pav</h5>
+        <p class="card-text">"One plate of Misal Pav = Instant happiness! Enjoy your feast!".</p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Home
